1d472f2ad45e190030425fdb156c2a4e1a656398
Khushal Mali <khushalmali8448@gmail.com>
2024-04-06 01:32:55 +0530

    Bugs fixed

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -82,7 +82,12 @@ const LoginModal = () => {
       <hr />
       <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} />
 
-      <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => {}} />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => signIn("github")}
+      />
 
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row items-center justify-center gap-2">
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // domains: ["avatars.githubusercontent.com"],
+
+    remotePatterns: [
+      {
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
