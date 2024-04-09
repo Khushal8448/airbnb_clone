d3e04cfbab486532ca7b6efe23ec879fe94ba08a
Khushal Mali <khushalmali8448@gmail.com>
2024-04-09 18:15:19 +0530

    added like unlike feature

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,6 +1,14 @@
 import type { User } from "@prisma/client";
+import type { Listing } from "@prisma/client";
 
-export type SafeUser = Omit<User, "createdAt" | "updatedAt" | "emailVerified"> & {
+export type SafeListing = Omit<Listing, "createdAt"> & {
+  createdAt: string;
+};
+
+export type SafeUser = Omit<
+  User,
+  "createdAt" | "updatedAt" | "emailVerified"
+> & {
   createdAt: string;
   updatedAt: string;
   emailVerified: string | null;
