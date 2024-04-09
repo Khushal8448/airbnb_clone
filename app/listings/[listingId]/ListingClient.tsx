a87acf1ee06c9bdc3e234a24f256382ec3e53f6a
Khushal Mali <khushalmali8448@gmail.com>
2024-04-10 01:25:14 +0530

    adding reservation functionality

---
+++ app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import axios from "axios";
+import Container from "@/app/components/Container";
+import ListingHead from "@/app/components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
+import { categories } from "@/app/components/navbar/Categories";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import type { SafeListing, SafeUser } from "@/app/types";
+import type { Reservation } from "@prisma/client";
+import { eachDayOfInterval } from "date-fns";
+import { useRouter } from "next/navigation";
+import { useMemo, useState, useCallback } from "react";
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: "selection",
+};
+
+interface ListingClientProps {
+  reservations?: Reservation[];
+  listing: SafeListing & {
+    user: SafeUser;
+  };
+  currentUser?: SafeUser | null;
+}
+
+const ListingClient: React.FC<ListingClientProps> = ({
+  reservations = [],
+  listing,
+  currentUser,
+}) => {
+  const loginModal = useLoginModal();
+  const router = useRouter();
+
+  const disabledDates = useMemo(() => {
+    let dates: Date[] = [];
+
+    reservations.forEach((reservation) => {
+      const range = eachDayOfInterval({
+        start: new Date(reservation.startDate),
+        end: new Date(reservation.endDate),
+      });
+
+      dates = [...dates, ...range];
+    });
+
+    return dates;
+  }, [reservations]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(listing.price);
+  const [dateRange, setDateRange] = useState(initialDateRange);
+
+  const onCreateReservation = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    setIsLoading(true);
+
+    axios
+      .post("/api/reservations", {
+        totalPrice,
+        startDate: dateRange.startDate,
+        endDate: dateRange.endDate,
+        listingId: listing?.id,
+      })
+      .then(() => {});
+  }, []);
+
+  const category = useMemo(() => {
+    return categories.find((item) => item.label === listing.category);
+  }, [listing.category]);
+
+  return (
+    <Container>
+      <div className="mx-auto max-w-screen-lg">
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+          <div className="mt-6 grid grid-cols-1 md:grid-cols-7 md:gap-10">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              bathroomCount={listing.bathroomCount}
+              guestCount={listing.guestCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default ListingClient;
